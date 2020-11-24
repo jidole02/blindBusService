e3654a3133ef71e50b1eaf6d9c3200d5f1815978
joenjihwan <s20062@gsm.hs.kr>
2020-11-24 13:36:50 +0900

    Struct adding..

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import SpeckOut from "./Components/SpeckOut";
 
-import MainContainer from './Container/MainContainer/MainContainer';
+import MainContainer from "./Container/MainContainer/MainContainer";
 
 const App = () => {
   return (
     <Router>
-      <Route exact path="/" component={MainContainer} />
-
+      {/* <Route exact path="/" component={MainContainer} /> */}
+      <Route exact path="/" component={SpeckOut} />
     </Router>
-  )
-}
+  );
+};
 
 export default App;
