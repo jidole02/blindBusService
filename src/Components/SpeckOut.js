aa02e26526bbf0aa298e21be5aad5ae3e6d1e112
joenjihwan <s20062@gsm.hs.kr>
2020-11-24 15:33:19 +0900

    Speckout_page except footer.

--- src/Components/SpeckOut.js
+++ src/Components/SpeckOut.js
@@ -1,26 +1,19 @@
 import React, { Component } from "react";
 import Mic from "./img/mic.png";
-import "./SpeckOutStyle.js";
+import * as B from "./Style";
 
 class SpeckOut extends Component {
   render() {
     return (
-      <section>
-        <div class="section_wrap">
-          <div class="section_container">
-            <img src={Mic} alt="Speck"/>
-            <div class="section_ment">
-                <div>최대한 또박또박 말해주세요</div>
-            </div>
-            <div class="sub_ment">
-                <div>Tell me as clearly as you can</div>
-            </div>
-          </div>
-          <div class="tts_value_view">
-              <input type="text" name="tts_value" disabled></input>
-          </div>
-        </div>
-      </section>
+      <B.Background>
+        <img src={Mic} alt="Speck" />
+        <span>최대한 또박또박 말해주세요</span>
+        <div>Tell me as clearly as you can</div>
+        <span id="value_wrap">첨단 09번</span>
+      </B.Background>
+    //   <B.footer>
+          
+    //   </B.footer>
     );
   }
 }
---
+++ src/Components/Style.js
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+const Background = styled.div`
+  width: 360px;
+  height: 640px;
+  @media screen and (min-width: 0px) and (max-width: 360px) and (max-height: 640px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    span {
+      margin-top: 30px;
+      font-size: 20px;
+    }
+    div {
+      font-size: 12px;
+      color: #c0c2d1;
+    }
+    #value_wrap {
+      margin-top: 36px;
+      width: 160px;
+      height: 30px;
+      border-radius: 25px;
+      outline: none;
+      border: none;
+      background-color: #2478ff;
+      filter: drop-shadow(0px 2px 4px rgba(76, 136, 251, 0.78));
+      padding: 5px 3px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: #ffff;
+      font-size: 14px;
+    }
+  }
+`;
+export { Background };
