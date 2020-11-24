0a5c279483eed5eee0c7a93e8dfce80cb807aaad
joenjihwan <s20062@gsm.hs.kr>
2020-11-24 14:24:38 +0900

    speckout_page [UPDATE].

--- src/Components/SpeckOut.js
+++ src/Components/SpeckOut.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import Mic from "./img/Speck_Group.png";
-import "./SpeckOutStyle.css";
+import Mic from "./img/mic.png";
+import "./SpeckOutStyle.js";
 
 class SpeckOut extends Component {
   render() {
@@ -9,6 +9,15 @@ class SpeckOut extends Component {
         <div class="section_wrap">
           <div class="section_container">
             <img src={Mic} alt="Speck"/>
+            <div class="section_ment">
+                <div>최대한 또박또박 말해주세요</div>
+            </div>
+            <div class="sub_ment">
+                <div>Tell me as clearly as you can</div>
+            </div>
+          </div>
+          <div class="tts_value_view">
+              <input type="text" name="tts_value" disabled></input>
           </div>
         </div>
       </section>
